8c53ce520ebfa8d3f6ddbfa8758e146635662e71
DiLeoEzequiel <ezequiel.dileo@efficientmind.consulting>
2024-11-12 16:42:17 -0300

    typeit

--- src/app/component/episodes/episodes.component.ts
+++ src/app/component/episodes/episodes.component.ts
@@ -1,11 +1,12 @@
-import { Component, HostListener } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, viewChild } from '@angular/core';
+import TypeIt from 'typeit';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrl: './episodes.component.css'
 })
-export class EpisodesComponent {
+export class EpisodesComponent implements AfterViewInit {
 
   //VARIABLES
   isScrolled = false;
@@ -18,6 +19,14 @@ export class EpisodesComponent {
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  ngAfterViewInit(): void {
+    new TypeIt("#simpleUsage", {
+        strings: "Buba duba lop lop, Morty! burp ¡No te hagas el tonto! Esa es la clave para… bueno, no importa, solo sigue diciendo buba duba lop lop y verás lo que pasa.",
+        speed: 50,
+        waitUntilVisible: true,
+    }).go();
   }
 
 }
